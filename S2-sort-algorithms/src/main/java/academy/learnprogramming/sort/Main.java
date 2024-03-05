d13356008f2f79590b1388fb001031dbb20e8b4a
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2024-03-04 22:27:16 -0800

    demo insertion sort and selection sort
    test successful

---
+++ S2-sort-algorithms/src/main/java/academy/learnprogramming/sort/Main.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming.sort;
+
+import sort.type.InsertionSort;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+/*      bubbleSort();
+        System.out.println("Finished bubble sort\n" );
+
+      SelectionSort.selectionSort();
+        System.out.println("Finished selection sort\n");*/
+
+        InsertionSort.insertionSort();
+
+
+
+    }
+}
--- S2-sort-algorithms/src/main/java/sort/type/BubbleSort.java
+++ S2-sort-algorithms/src/main/java/sort/type/BubbleSort.java
@@ -1,15 +1,15 @@
-package academy.learnprogramming.bubblesort;
+package sort.type;
 
-public class Main {
+public class BubbleSort {
 
-    public static void main(String[] args){
+    public static void bubbleSort(){
 
         // Demo of bubble sort (stable)
 
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
 
         for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;
-                lastUnsortedIndex--){
+             lastUnsortedIndex--){
 
             for(int i = 0; i < lastUnsortedIndex; i++ ){
                 if (intArray[i] > intArray[i + 1]){
