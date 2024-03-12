550e8fe599656a0acc4123cb10f5784ea6a648b4
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2024-03-11 21:48:07 -0700

    demo shell sort with update
    test successful

--- S2-sort-algorithms/src/main/java/academy/learnprogramming/sort/Main.java
+++ S2-sort-algorithms/src/main/java/academy/learnprogramming/sort/Main.java
@@ -9,13 +9,14 @@ public class Main {
 /*      bubbleSort();
         System.out.println("Finished bubble sort\n" );
 
-      SelectionSort.selectionSort();
+        SelectionSort.selectionSort();
         System.out.println("Finished selection sort\n");*/
 
         //InsertionSort.insertionSort();
+        //System.out.println("Finished Insertion sort sort\n")
 
         ShellSort.shellSort();
-
+        System.out.println("Finished Shell sort sort\n");
 
 
     }
